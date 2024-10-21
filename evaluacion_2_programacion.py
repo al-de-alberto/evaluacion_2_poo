1d5884cdfcae307c206b117ef058faa3566cb246
milosita <138174872+99milo@users.noreply.github.com>
2024-10-21 09:14:30 -0300

    Update evaluacion_2_programacion.py

--- evaluacion_2_programacion.py
+++ evaluacion_2_programacion.py
@@ -1,3 +1,17 @@
 #Bienvenidos a la evaluacion 2 en GitHub!
+import os
 
-print("Bienvenido al Software del Colegio!")
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pausa():
+    input("Presione Enter para continuar...")
+
+def mostrar_menu():
+    limpiar_pantalla()
+    print("\nSistema de Gestión Escolar")
+    print("1.Crear")
+    print("2. Leer")
+    print("3. Actualizar")
+    print("4. Eliminar")
+    print("5. Salir")
